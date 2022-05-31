5846863f0b5acea5f154c39499987010896c6017
anshika62003 <106245495+anshika62003@users.noreply.github.com>
2022-05-31 14:14:24 +0530

    Create pattern5.py

---
+++ pattern5.py
@@ -0,0 +1,17 @@
+n=int(input("Enter the number columnns"))
+for i in range(1,n+1):
+    for j in range(i,n):
+        print(" ",end="")
+    for k in range(1,i+1):
+        print("*",end="")
+    print()
+for i in range(1,n+1):
+    for z in range(1,n+1):
+        print(" ",end="")
+    for k in range(i,n+1):
+        print("*",end="")
+    for j in range(1,i):
+        print(" ",end="")
+© 2022 GitHub, Inc.
+Terms
+P
